001ca5cd98f660cba7bc19c99601435b39075f20
Kao Thao <kausthoj@gmail.com>
2016-04-05 23:13:29 +0000

    if else excercise

--- js/script.js
+++ js/script.js
@@ -1,80 +1,82 @@
-var create_input_tag;
-var read_input_tag;
-var update_input_tag;
-var delete_input_tag;
+var button;
+var input_box;
 
-var create_element = function(element_name){
+var change_font_size = function(font_size_amount){
     
-    var element = document.createElement(element_name);
-    element.style.display = 'block';
+    if(button.style.fontSize === '100px'){
+        
+        button.style.fontSize = '10px';
+        button.style.backgroundColor = 'yellow';
+        document.body.backgroundColor = 'pink';
+        
+    }else{
+        
+        button.style.fontSize = '100px';
+        button.style.backgroundColor = 'coral';
+        document.body.backgroundColor = 'pink';
+    };
     
-    return element;
 };
 
-var attach_element = function(element_child){
+var create_body_element = function(element_name){
     
-    document.body.appendChild(element_child);
+    var element = document.createElement('p');
+    element.textContent = 'what';
+    element.style.backgroundColor = 'white';
+    
+    document.body.appendChild(element);
+    
+    element.addEventListener('mouseover',function(event){
+        
+        if(element.style.backgroundColor === 'white'){
+            
+            element.style.backgroundColor = 'black';
+            
+        }else if(element.style.backgroundColor === "black"){
+            
+            element.style.backgroundColor = 'red';
+            
+        }else{
+            element.style.backgroundColor = 'white';
+        };
+        
+    });
+    
 };
+
 
 
 document.addEventListener('DOMContentLoaded',function(event){
     
+    button = document.getElementById('big_button');
+    button.style.fontSize = '100px';
     
-    
-    
-    create_input_tag = create_element('input');
-    read_input_tag = create_element('input');
-    update_input_tag = create_element('input');
-    delete_input_tag = create_element('input');
-    
-    create_input_tag.placeholder = 'create text goes here';
-    read_input_tag.placeholder = 'read text goes here';
-    update_input_tag.placeholder = 'update text goes here';
-    delete_input_tag.placeholder = 'delete text goes here';
-    
-    attach_element(create_input_tag);
-    attach_element(read_input_tag);
-    attach_element(update_input_tag);
-    attach_element(delete_input_tag);
-    
-    create_input_tag.addEventListener('keydown',function(event){
+    button.addEventListener('click',function(event){
         
-        if(event.keyCode === 13){
-            
-            var new_element = document.createElement('ul');
-            new_element.setAttribute('id', create_input_tag.value);
-            new_element.textContent = create_input_tag.value;
-            this.value = null;
-            
-            attach_element(new_element);
-            
-            
-        };
-        
+        change_font_size();
     });
+    
+    input_box = document.createElement('input');
+    input_box.placeholder = 'input data then press enter';
+    input_box.style.fontSize = '15px';
+    
+    
+    document.body.appendChild(input_box);
+    
+    input_box.addEventListener('keydown',function(event){
         
-    update_input_tag.addEventListener('keydown',function(event){
         
-        if(event.keyCode === 13){
-            
-            var element_to_update = document.getElementById(update_input_tag.value);
-            element_to_update.style.backgroundColor = this.value;
-            this.value = null;
-            
+    if(event.keyCode === 13){
+        
+        this.value = null;
+        
+        create_body_element();
+        
         };
         
     });
     
-    delete_input_tag.addEventListener('keydown',function(event){
-        
-        if(event.keyCode === 13){
-            var element_to_delete = document.getElementById(this.value);
-            element_to_delete.parentNode.removeChild(this.li_tag);
-            this.value = null;
-        };
-        
-        
-    });
+    
+    
     
 });
-
