56da31a871a972ea66b9f709daf9bead6c27a486
Kao Thao <kausthoj@gmail.com>
2016-04-15 18:10:41 +0000

    updated calc for demo

--- js/script.js
+++ js/script.js
@@ -1,63 +1,121 @@
-var github_emojis;
+var section_tag;
+var display_box;
+var number_buttons_div;
+var plus_minus_div;
+var plus_button;
+var minus_button;
+var equal_button;
 
-var http_request = new XMLHttpRequest();
 
-http_request.open('GET','https://api.github.com/emojis',false);
-
-http_request.onreadystatechange = function(){
+    section_tag = document.createElement('section')
+    section_tag.style.backgroundColor = 'grey';
+    section_tag.style.width = '300px';
+    section_tag.style.height = '400px';
+    section_tag.style.borderRadius = '5px';
+    section_tag.style.display = 'inline-block';
+    section_tag.style.marginTop = '5rem';
     
-    if(this.readyState === this.DONE && this.status === 200){
+    display_box = document.createElement('div');
+    display_box.setAttribute('id','display_box');
+    
+    number_buttons_div = document.createElement('div');
+    number_buttons_div.setAttribute('id','numbers_div');
+    
+    plus_minus_div = document.createElement('div');
+    plus_minus_div.setAttribute('id', 'plus_and_minus_div');
+    
+    plus_button = document.createElement('button');
+    plus_button = document.createElement('button');
+    plus_button.style.width = '90%';
+    plus_button.style.height = '40%';
+    plus_button.textContent = '+';
+    plus_button.style.fontSize = '2.5rem';
+    plus_button.style.borderRadius = '5px';
+    
+    minus_button = document.createElement('button');
+    minus_button = document.createElement('button');
+    minus_button.style.width = '90%';
+    minus_button.style.height = '40%';
+    minus_button.textContent = '-';
+    minus_button.style.fontSize = '2.5rem';
+    minus_button.style.borderRadius = '5px';
+    
+    equal_button = document.createElement('button');
+    equal_button = document.createElement('button');
+    equal_button.style.width = '80%';
+    equal_button.style.height = '50px';
+    equal_button.textContent = '=';
+    equal_button.style.fontSize = '2.5rem';
+    equal_button.style.borderRadius = '5px';
+    
+    plus_button.addEventListener('click',function(event){
+                
+                var plus_sign = document.createElement('span');
+                plus_sign.textContent = '+';
+                display_box.appendChild(plus_sign);
+                
+    });
+    
+    minus_button.addEventListener('click',function(event){
+                
+                var minus_sign = document.createElement('span');
+                minus_sign.textContent = '-';
+                display_box.appendChild(minus_sign);
+                
+    });
+    
+    // equal_button.addEventListener('click',function(event){
+                
+    //             var equal_sign = document.createElement('span');
+    //             equal_sign.textContent = '=';
+    //             display_box.appendChild(equal_sign);
+                
+    // });
+
+    var create_number_buttons = function(amount_of_number_buttons){
         
-        github_emojis = JSON.parse(this.response);
+        var number_buttons;
+        
+        for(var i=1; i<=amount_of_number_buttons; i++){
+            number_buttons = document.createElement('button');
+            number_buttons.setAttribute('id','number_'+i)
+            number_buttons.textContent = i;
+            number_buttons.style.width = '45%';
+            number_buttons.style.height = '40%';
+            number_buttons.style.fontSize = '2.5rem';
+            number_buttons.style.borderRadius = '5px';
+            
+            number_buttons.addEventListener('click',function(event){
+                
+                var numbers = document.createElement('span');
+                numbers.textContent = this.innerHTML;
+                display_box.appendChild(numbers);
+                
+            });
+            
+            number_buttons_div.appendChild(number_buttons);
+        };
+        
+        equal_button.addEventListener('click',function(event){
+                
+                var equal_sign = document.createElement('span');
+                equal_sign.textContent = '=';
+                display_box.appendChild(equal_sign);
+                
+    });
         
     };
     
-};
-
-var get_random_emoji = function(){
-    
-    var total_number_of_emojis = Object.keys(github_emojis).length;
-    var random_number = Math.round(Math.random()*total_number_of_emojis);
-    var emoji_name = Object.keys(github_emojis)[random_number];
-    
-    var emoji_urls = github_emojis[emoji_name];
-    return emoji_urls;
-    
-};
-
-var get_emoji_urls = function (amount_of_urls){
-    
-    var total_number_of_emojis = Object.keys(github_emojis).length;
-    
-    for(var i=0; i<amount_of_urls; i++){
-        
-        var emoji_name = Object.keys(github_emojis)[i];
-        var emoji_urls = github_emojis[emoji_name];
-        
-        var image = document.createElement('img');
-        
-        image.addEventListener('click',function(event){
-            
-            this.setAttribute('src',get_random_emoji());
-            console.log(this);
-            this.style.opacity = '.5';
-        });
-        
-        image.addEventListener('dblclick',function(event){
-            
-            this.remove();
-            
-        });
-        
-        image.setAttribute('src',emoji_urls);
-        document.body.appendChild(image);
-    };
-    
-};
-
-
 document.addEventListener('DOMContentLoaded',function(event){
     
-    http_request.send(null);
-    get_emoji_urls(10);
+    document.body.appendChild(section_tag);
+    section_tag.appendChild(display_box);
+    section_tag.appendChild(number_buttons_div);
+    section_tag.appendChild(plus_minus_div);
+    section_tag.appendChild(equal_button);
+    plus_minus_div.appendChild(plus_button);
+    plus_minus_div.appendChild(minus_button);
+    
+    create_number_buttons(4);
+    
 });
