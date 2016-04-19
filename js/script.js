a1e574902bfd05bfa61778cccfcf5ce060e47fda
Kao Thao <kausthoj@gmail.com>
2016-04-19 00:08:12 +0000

    pushing latest on calc

--- js/script.js
+++ js/script.js
@@ -5,45 +5,55 @@ var answer_container;
 var numbers_container;
 var operators_container;
 var input_element;
+var image;
 
 calc_number_buttons = [
     
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
     7,
     8,
     9,
+    4,
+    5,
+    6,
+    1,
+    2,
+    3,
     0
     
 ];
 
 calc_operator_buttons = [
     
-    '+',
-    '-',
     '*',
+    '-',
+    '+',
     '/'
     
 ];
 
 calc_body = document.createElement('div');
-calc_body.style.backgroundColor = 'coral';
-calc_body.style.width = '500px';
-calc_body.style.height = '600px';
+calc_body.style.backgroundColor = '#ECEAEA';
+calc_body.style.width = '400px';
+calc_body.style.height = '500px';
 calc_body.style.borderRadius = '10px';
+calc_body.style.display = 'inline-block';
+calc_body.style.boxShadow = '2px 2px 2px grey';
 
 input_element = document.createElement('input');
 input_element.setAttribute('id','answer_container');
+// input max is not working
+// input_element.setAttribute('max','10');
+// input_element.setAttribute('type','number');
 
 numbers_container = document.createElement('div');
 numbers_container.setAttribute('id','numbers_container');
 
 operators_container = document.createElement('div');
 operators_container.setAttribute('id','operators_container');
+
+image = document.createElement('img');
+image.setAttribute('src','http://i.imgur.com/l72MAu7.png');
+image.setAttribute('id','calc_image');
 
 var create_numbers_element = function(element_type, element_id){
     
@@ -69,6 +79,7 @@ document.addEventListener('DOMContentLoaded',function(event){
     calc_body.appendChild(input_element);
     calc_body.appendChild(numbers_container);
     calc_body.appendChild(operators_container);
+    calc_body.appendChild(image);
     
     answer_container = document.getElementById('answer_container');
     
@@ -113,7 +124,9 @@ document.addEventListener('DOMContentLoaded',function(event){
     var clear_data_button = document.getElementById('clear_data_button');
     
     equal_button.textContent = '=';
+    equal_button.style.backgroundColor = 'grey';
     clear_data_button.textContent = 'AC';
+    clear_data_button.style.backgroundColor = 'orangered';
     
     calc_body.appendChild(numbers_container);
     calc_body.appendChild(operators_container);
